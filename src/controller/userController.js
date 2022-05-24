32ef379420c153a0fa9f12febc4e169f2171e593
MohitRaykwar7389966852 <mohitraykwar99@gmail.com>
2022-05-24 16:16:57 +0530

    create user

---
+++ src/controller/userController.js
@@ -0,0 +1,79 @@
+const userModel = require('../model/userModel')
+const aws= require("aws-sdk")
+
+aws.config.update({
+    accessKeyId: "AKIAY3L35MCRUJ6WPO6J",
+    secretAccessKey: "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",
+    region: "ap-south-1"
+})
+
+let uploadFile= async (file) =>{
+    return new Promise( function(resolve, reject) {
+     let s3= new aws.S3({apiVersion: '2006-03-01'});
+
+     var uploadParams= {
+         ACL: "public-read",
+         Bucket: "classroom-training-bucket",
+         Key: file.originalname,
+         Body: file.buffer
+     }
+    
+     s3.upload( uploadParams, function (err, data ){
+         if(err) return reject({"error": err})
+         return resolve(data.Location)
+     })
+    })
+ }
+
+const isValid = function (value) {
+    if (typeof value === "undefined" || value === null) return false;
+    if (typeof value === "string" && value.trim().length === 0) return false;
+    return true;
+  };
+
+const createUser = async function(req,res)
+{
+    try{
+            let files = req.files
+            let profileImage = files[0]
+            if(!isValid(profileImage)) return res.status(400).send({ status: false, message: "Please Enter Profile Image" });
+            
+            let data =req.body.data
+            if(!isValid(data)) return res.status(400).send({ status: false, message: "No Data Found" });
+            
+            let input = JSON.parse(data) 
+
+            let {fname,lname,email,phone,password,address} = input
+
+            if(!isValid(fname)) return res.status(400).send({ status: false, message: "Please Enter First Name" });
+            if(!isValid(lname)) return res.status(400).send({ status: false, message: "Please Enter Last Name" });
+
+            if(!isValid(email)) return res.status(400).send({ status: false, message: "Please Enter Email" });
+            const checkEmail = await userModel.findOne({ email: email });
+            if (checkEmail) return res.status(409).send({ status: false, message: "Email is already register" });
+            if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.trim())) return res.status(400).send({ status: false, message: "Email should be valid" });
+            
+            if(!isValid(phone)) return res.status(400).send({ status: false, message: "Please Enter Phone Number" });
+            const checkPhone = await userModel.findOne({ phone: phone });
+            if (checkPhone) return res.status(409).send({status: false,message: "Mobile number is already registered",});
+            if (!/^\d{10}$/.test(phone)) return res.status(400).send({ status: false, message: "Mobile no should be valid" });
+
+            if(!isValid(password)) return res.status(400).send({ status: false, message: "Please Enter Password" });
+            if (password.length < 8 || password.length > 15) return res.status(400).send({status: false,message: "password length should be in the range of 8 to 15 only",});
+            
+            // if(!isValid(address.shipping)) return res.status(400).send({ status: false, message: "Please Enter Shipping Address" });
+            // if(!isValid(address.billing)) return res.status(400).send({ status: false, message: "Please Enter billing Address" });
+
+            let imageUrl = await uploadFile(profileImage)
+            input.profileImage = imageUrl
+
+            let userCreated = await userModel.create(input)
+            return res.status(201).send({status:true , data:userCreated})
+    }
+    catch(e)
+    {
+        res.status(500).send({status:false , message:e.message})
+    }
+}
+
+module.exports = {createUser}
---
+++ src/model/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        trim:true,
+        required: true
+    },
+    lname: {
+        type: String,
+        trim:true,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim:true,
+    },
+    profileImage: {
+        type:String,
+        trim:true,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+        trim:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:8,
+        maxlength:15,
+        trim:true
+    },
+    address: {
+        shipping: {
+          street: {type:String, required:true},
+          city: {type:String, required:true},
+          pincode: {type:Number, required:true}
+        },
+        billing: {
+            street: {type:String, required:true},
+            city: {type:String, required:true},
+            pincode: {type:Number, required:true}
+        }
+    }
+
+},{timestamps: true });
+
+module.exports = mongoose.model('User',userSchema)
